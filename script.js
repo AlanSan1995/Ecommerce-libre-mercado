50985aa6b9a09ea9eafbe2d6a00487538e854229
Alan San <alansanchez882@gmail.com>
2023-12-27 15:56:37 -0300

    se agrego modo lista y modo cuadricula

--- script.js
+++ script.js
@@ -67,4 +67,75 @@ categoriesDropdownButton.onclick = async()=>{
 }
 
 
+const productsCall = async (category = null)=>{
+    try {
+        const response = await fetch(`https://fakestoreapi.com/products${category ? `/category/${category}`:''}`)
+        const data = await response.json()
+        return data
+    } catch (error) 
+    { 
+        console.error(error);
+    }
+}
+const productCount = document.getElementById('productCount')
+
+
+const createRowProducts = async ()=>{
+    const productList = document.getElementById('productList')
+    productList.innerHTML =''
+    const products = await productsCall()
+    products.forEach(prod => {
+        productList.innerHTML += `<div class="product_row" >
+        <div class="product_row_img"><img src="${prod.image}" alt="" /></div>
+        <div class="product_row_content">
+          <h2>${prod.title}</h2>
+          <p class="product_row_price">$${prod.price}</p>
+          <p class="product_row_description">
+            ${prod.description}
+          </p>
+          <a href="">View details</a>
+        </div>
+      </div>`
+    });
+    productCount.innerHTML = `${products.length} items in <b>Mobile accessory</b>`
+}
+createRowProducts()
+
+const createCardProducts = async ()=>{
+    const productList = document.getElementById('productList')
+    productList.innerHTML =''
+    const products = await productsCall()
+    products.forEach(prod => {
+        productList.innerHTML += `<div class="product_card">
+        <div class="product_card_img"><img src="${prod.image}" alt="" /></div>
+        <div class="product_card_content">
+          <p class="product_card_price">$${prod.price}</p>
+          <h2>${prod.title}</h2>
+          <a href="">View details</a>
+        </div>
+      </div>`
+    });
+    productCount.innerHTML = `${products.length} items in <b>Mobile accessory</b>`
+
+}
+
+const productsStyleButtonCard = document.getElementById('productsStyleButtonCard')
+
+const productsStyleButtonList = document.getElementById('productsStyleButtonList')
+
+
+
+productsStyleButtonCard.onclick = async ()=>{
+    await createCardProducts()
+    productsStyleButtonCard.classList.toggle('products_style_button_card_active')
+    productsStyleButtonList.classList.toggle('products_style_button_list_active')
+}
+
+productsStyleButtonList.onclick =async ()=>{
+    await createRowProducts()
+    productsStyleButtonCard.classList.toggle('products_style_button_card_active')
+    productsStyleButtonList.classList.toggle('products_style_button_list_active')
+}
+
+
 
