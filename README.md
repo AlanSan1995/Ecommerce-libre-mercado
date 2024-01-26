ea4243eaf0c184bbe007e0f0ab2082d8d4d2fe24
AlanSan1995 <alansanchez882@gmail.com>
2024-01-26 15:34:50 -0300

    se agrego la documentacion y el readme al proyecto

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proyecto final de Javascript - CoderHouse
+
+Es una tienda online la cual simula un entorno en el cual podes interactuar con los productos , agregandolos al carrito y aumentar o disminuir la cantida de articulos por comprar.
+tambien se simulan las categorias de los productos la cual sirve como filtro para los mismos.
+llenado el carritose puede ver los productos removerlos o sumar mas, simular la compra poniendo los datos para ser enviados o contactado, tambien se puede ver el total de compra y finaliar la misma.
+
+## Bibliotecas Utilizadas
+- Sweetalert2 version 11.10.4
--- script.js
+++ script.js
@@ -15,22 +15,24 @@ const categoriesCall = async()=>{
 const dropdownButton = document.getElementById('dropdownButton')
 
 dropdownButton.onclick = async()=>{
-    
+    // A partir de aca se insertan los botones de las categorias
     const dropdown = document.getElementById('dropdown')
     if (!dropdown.classList[1]) {
-        const categories = await categoriesCall()
+        const categories = await categoriesCall() //se utiliza la funcion categoriesCall con async/await
         dropdown.innerHTML=''
         categories.forEach(category => {
             dropdown.innerHTML += `<button>${category}</button>`
         });
     }
+    
+    // aca se da vuelta la flecha del Dropdown 
 
     const dropdownButtonArrow = document.getElementById('dropdownButtonArrow')
     dropdownButtonArrow.classList.toggle('dropdown_button_arrow_show')
     dropdown.classList.toggle('dropdown_show')
 }
 
-// Crea categorias en mobile
+// Crea categorias en mobile (esto se hace para que tengan un estilo diferente al Desktop)
 
  const createCategoryList = async ()=>{
     const categoryList = document.getElementById('categoryList')
@@ -45,21 +47,25 @@ if (document.body.clientWidth <= 480 ) {
     createCategoryList()
 }
 
-//Pintan las cards de los productos trayendo desde la api
-
+// Se traen los productos del local Storage en caso de que existan 
 let cartProducts = JSON.parse(localStorage.getItem('cartProducts')) || []
 
+// con esta funcion agregamos productos al carrito 
 const addProductToCart = async (pid)=>{
     const productButton = document.getElementById(`productButton${pid}`)
-    productButton.disabled = true
+    productButton.disabled = true // Se deshabilita el boton que ejecuta la funcion para evitar errores de asincronia
+    
+    //se verifica si existe el elemento para sumar su cantidad 
+    //en caso de existir o sumar todos sus datos en caso de que no sea asi 
     if (!cartProducts.some((elem)=>elem.product.id == pid )) {
-        const response = await fetch(`https://fakestoreapi.com/products/${pid}`)
+        const response = await fetch(`https://fakestoreapi.com/products/${pid}`) // se trae el producto con el id especificado
         const data = await response.json()
         cartProducts.push({
             quantity:1,
             product : data
         })   
     }else{
+        //se busca el elemento y se suma uno a su cantidad 
         cartProducts.map((elem)=>{
 
             if (elem.product.id == pid) {
@@ -68,11 +74,12 @@ const addProductToCart = async (pid)=>{
             return elem
         })
     }
-    localStorage.setItem('cartProducts',JSON.stringify(cartProducts))
+    localStorage.setItem('cartProducts',JSON.stringify(cartProducts)) // se guarda el carrito actualizado en el local Storage
     createCart()
     productButton.disabled = false
 }
 
+// funcion para traer todos los productos y filtrarlos por categorias de ser encesario
 const productsCall = async (category = null)=>{
     try {
         const response = await fetch(`https://fakestoreapi.com/products${category ? `/category/${category}`:''}`)
@@ -83,13 +90,14 @@ const productsCall = async (category = null)=>{
         console.error(error);
     }
 }
-const productCount = document.getElementById('productCount')
 
+const productCount = document.getElementById('productCount') // elemento que muestra la cantidad de productos de la pagina
 
-const createRowProducts = async (category = null)=>{
+// Se crean los productos en su version de lista
+const createRowProducts = async (category = null)=>{    
     const productList = document.getElementById('productList')
-    productList.innerHTML =''
-    const products = await productsCall(category)
+    productList.innerHTML ='' 
+    const products = await productsCall(category) // Se utiliza lafuncion productCall para obtener los productos
     products.forEach(prod => {
         productList.innerHTML += `<div class="product_row" >
         <div class="product_row_img"><img src="${prod.image}" alt="" /></div>
@@ -103,10 +111,11 @@ const createRowProducts = async (category = null)=>{
         </div>
       </div>`
     });
-    productCount.innerHTML = `${products.length} items in <b>Mobile accessory</b>`
+    productCount.innerHTML = `${products.length} items in <b>Mobile accessory</b>` // aca se cambia la cantidad de elementos
 }
 createRowProducts()
 
+// esta funcion crea los productos en su version de cards
 const createCardProducts = async (category = null)=>{
     const productList = document.getElementById('productList')
     productList.innerHTML =''
@@ -128,6 +137,7 @@ const createCardProducts = async (category = null)=>{
 
 let productsStyle = 'list'
 
+// con esta funcion se filtra los productos por categorias 
 const  filterByCategory = async (category)=>{
     if (productsStyle == 'list') {
         createRowProducts(category.replace(/`/g, "'"))   
@@ -136,8 +146,10 @@ const  filterByCategory = async (category)=>{
     }
 }
 
+// este es el elemento  dropdown para mostrar las categorias 
 const categoriesDropdownButton = document.getElementById('categoriesDropdownButton')
 
+// se agrega un onlick para que se muestren o no las categorias del dropdown 
 categoriesDropdownButton.onclick = async()=>{
     const dropdown = document.getElementById('categoriesDropdown')
     if (!dropdown.classList[1]) {
@@ -152,10 +164,13 @@ categoriesDropdownButton.onclick = async()=>{
     dropdown.classList.toggle('categories_dropdown_show')
 }
 
+// botones que cambian los estilos de los productos (lista o cards)
+
 const productsStyleButtonCard = document.getElementById('productsStyleButtonCard')
 
 const productsStyleButtonList = document.getElementById('productsStyleButtonList')
 
+// funciones que cambian los estilos de los productos (lista o cards)
 productsStyleButtonCard.onclick = async ()=>{
     await createCardProducts()
     productsStyle = 'card'
@@ -170,29 +185,44 @@ productsStyleButtonList.onclick =async ()=>{
     productsStyleButtonList.classList.toggle('products_style_button_list_active')
 }
 
-//
-
+// Se elimina un producto del carrito
 const deleteProductFromCart = (pid)=>{
     cartProducts = cartProducts.filter((elem)=>elem.product.id != pid)
     localStorage.setItem('cartProducts',JSON.stringify(cartProducts))
     createCart()
 }
+
+// se asigna un onclick a cada boton de eliminar del carrito con la funcionalidad de deleteProductFromCart
+const onclicksForCartDeleteButtons = ()=>{
+    const deleteProductFromCartButtons = document.querySelectorAll('.delete_product_from_cart_button')
+
+    deleteProductFromCartButtons.forEach((button)=>{
+        button.onclick = ()=>deleteProductFromCart(button.id)
+    })
+}
+
+// con esta funcion se eliminan todos los productos del carito 
 const deleteAllProductFromCart = ()=>{
     cartProducts = []
     localStorage.setItem('cartProducts',JSON.stringify(cartProducts))
     createCart()
 }
 
+// Boton para eliminar todo del carrito 
 const removeAllCart = document.getElementById('removeAllCart')
 
 removeAllCart.onclick = deleteAllProductFromCart
 
+// Contenedor de los productos 
 const cartProductsContainer = document.getElementById('cartProducts')
 
+// elemento que muestra el subtotal de la compra 
 const cartSubtotal = document.getElementById('cartSubtotal')
 
+// elemento que muestra el total de la compra 
 const cartTotal = document.getElementById('cartTotal')
 
+// Con esta funcion se suma uno en la cantidad del producto especificado con el pid
 const addQuantity = (pid)=>{
     cartProducts = cartProducts.map((elem)=>{
         if (elem.product.id == pid) {
@@ -203,6 +233,7 @@ const addQuantity = (pid)=>{
     localStorage.setItem('cartProducts',JSON.stringify(cartProducts))
     createCart()
 }
+// Con esta funcion se resta uno en la cantidad del producto especificado con el pid
 const removeQuantity = (pid)=>{
     cartProducts = cartProducts.map((elem)=>{
         if (elem.product.id == pid) {
@@ -216,7 +247,7 @@ const removeQuantity = (pid)=>{
     createCart()
 }
 
-
+// aca se agrega al html todos los productos que esten dentro del array del carrito
 const createCartProducts = ()=>{
     cartProductsContainer.innerHTML = ''
     cartProducts.forEach(elem => {
@@ -231,7 +262,7 @@ const createCartProducts = ()=>{
                   </div>
                   <div class="cart_product_title">
                     <h3>${elem.product.title}</h3>
-                    <button onclick='deleteProductFromCart(${elem.product.id})' >Remove</button>
+                    <button class="delete_product_from_cart_button" id="${elem.product.id}">Remove</button>
                   </div>
                 </div>
                 <div class="cart_product_pricing">
@@ -245,12 +276,13 @@ const createCartProducts = ()=>{
               </div>
             <span class="cart_separator"></span> 
         `
-     
     });
+    onclicksForCartDeleteButtons() // despues de creados los productos se agregan los onclicks a los botones de remover 
     cartSubtotal.innerHTML = cartProducts.reduce((acc,elem)=>acc + elem.quantity * elem.product.price,0).toFixed(2)
     cartTotal.innerHTML = cartProducts.reduce((acc,elem)=>acc + elem.quantity * elem.product.price,0).toFixed(2)
 }
 
+// se crean los estilos del carrito vacio 
 const createEmptyCart= ()=>{
     cartProductsContainer.innerHTML = `<div class="cart_empty">
     <img src="./assets/img/bolsas-de-compra.png" alt="Blsa de compras">
@@ -261,6 +293,7 @@ const createEmptyCart= ()=>{
   cartTotal.innerHTML = 0
 }
 
+// se decide si crean los productos o el carrito vacio dependiendo de la cantidad de productos en el array de carrito
 const createCart = ()=>{
     if (cartProducts.length == 0) {
         return createEmptyCart()
@@ -269,10 +302,12 @@ const createCart = ()=>{
 }
 createCart()
 
+// Boton de finalizar la compra
 const finishSale = document.getElementById('finishSale')
 
-
+// funcionalidad de finalizacion de la compra 
 finishSale.onclick = async ()=>{
+    // se muestra un error en caso de que no haya productos en el carrito
     if (cartProducts.length == 0) {
         return Swal.fire({
             title: "Error en su compra",
@@ -280,6 +315,7 @@ finishSale.onclick = async ()=>{
             icon: "error"
           });
     }
+    // se crea con sweet alert un formulario para ingresar datos del usuario 
     const { value: formValues } = await Swal.fire({
         title: "Rellena el formulario para terminar la compra",
         html: `
@@ -296,6 +332,7 @@ finishSale.onclick = async ()=>{
           }
         }
       });
+      // se verifica si el formulario esta completo y se termina la compra en caso de ser asi.
       if (formValues.name != "" || formValues.email != "" || formValues.phone != "") {
         Swal.fire({
             icon: "success",
@@ -313,7 +350,7 @@ finishSale.onclick = async ()=>{
             icon: "error"
           });
       }
-    deleteAllProductFromCart()
+    deleteAllProductFromCart()// se eliminan todos los productos del carrito
 }
 
 
